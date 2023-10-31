32b375aebc3993e6b044dc752ce8058a6e9d2243
edwardtimsack <edwardtimothysackie@gmail.com>
2023-10-31 12:05:39 +0000

    completed the add and display function

---
+++ todo.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+    echo "Welcome To Our TodoList Application"
+    echo ""
+    echo "Press 1 To Add New Todo"
+	echo "Press 2 To Edit Existing Todo"
+	echo "Press 3 To Display All Todos"
+	echo "Press 4 To Delete"
+	echo "Press 5 To Search For Todo"
+    read -p "Select a number to perform a task: " user_input
+# Function to add new todo
+
+function add_Todo() {
+    echo "Enter new todo: "
+    read new_todo
+    echo "$new_todo" >> todo_list.txt
+    echo "You have added a new todo: $new_todo"
+}
+
+
+
+
+# Function to edit existing todo
+# function edit_Todo() {
+    
+# }
+
+
+
+
+# Function to display todo
+function display_Todo() {
+    echo "Todo List:"
+    cat todo_list.txt
+
+}
+
+
+
+
+# Function to delete / remove todo
+# function delete_Todo() {
+
+# }
+
+
+
+
+# Function to search todo
+# function search_Todo() {
+
+# }
+
+
+# Checking_User_Input
+
+if [ $user_input -eq 1 ]
+then
+add_Todo
+elif [ $user_input -eq 2 ]
+then
+edit_Todo
+elif [ $user_input -eq 3 ]
+then
+display_Todo
+elif [ $user_input -eq 4 ]
+then
+delete_Todo
+elif [ $user_input -eq 5 ]
+then
+search_Todo
+else
+echo "Invalid Input, Try Again"
+fi
